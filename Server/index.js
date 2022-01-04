471065b5328da66d9ee8e9d28ddb452452bb8016
Justin Pooters <justin@wiseconn.nl>
2022-01-04 14:22:04 +0100

    Added features and removed uselsss features

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const config = require("./config.json");
+const servers = require("./servers.json")
 const port = config.port;
 const bodyParser = require('body-parser');
 const os = require('os');
+var osutils = require('os-utils');
+
+
+// servers.forEach(server => {
+// })
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
